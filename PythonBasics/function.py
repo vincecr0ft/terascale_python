82aa65ca72e35d3d2849d71560c261dc9a56efa8
vcroft <vincent.croft@cern.ch>
2018-10-20 16:05:46 +0200

    initial upload

---
+++ PythonBasics/function.py
@@ -0,0 +1,4 @@
+def Get_Primes(some_range):
+    print("I don't know how!")
+    results = {"largest prime":2, "Commonest Factor":"eggs","largest factor":2}
+    return results
---
+++ README.md
@@ -0,0 +1,1 @@
+# Vince's terrascale notes
